d66374f62e3b3d33a68eec1712984ca6835903f0
David Kirkby <dkirkby@uci.edu>
2021-03-05 17:57:42 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# US Citizenship Civics Test Study Aid
+
+A text-based tool to ask random questions from the [2008 Civics Test](https://www.uscis.gov/sites/default/files/document/questions-and-answers/100q.txt).  After each question, hit RETURN to see the answers then assign a weight 0-9 to this question. Questions with higher weights will be asked more frequently and a weight of 0 prevents a question ever being asked.
+
+The first time you run this script, all questions will have weight 5.  Weights are stored in a file `weights.npy` that is updated after each question (with a backup saved as `weights.npy.bak`).  Rename or delete this file to reset all weights to 5.  When you exit the script (by entering `q`) a file `sorted.txt` is written containing the 100 questions in order decreasing weight.
+
+The questions are stored in a plain text file `questions.txt`. Some answers depend on your current elected officials but you can edit this file to make any changes.  The current answers are for California's 45th congressional district in 2021.
+
+Good luck with your test!
+
+## Instructions
+
+To make a local copy of the questions and python script use:
+```
+git clone https://github.com/dkirkby/civics.git
+cd civics
+```
+To run the script use:
+```
+python civics.py
+```
---
+++ civics.py
@@ -0,0 +1,99 @@
+import re
+import pathlib
+import random
+import shutil
+
+
+def load_questions(name='questions.txt'):
+    # Read the questions.
+    qpat = re.compile('^([0-9]+)\. (.*)$')
+    apat = re.compile('^ - (.*)$')
+    questions = []
+    with open('questions.txt') as f:
+        for i, line in enumerate(f):
+            line = line.rstrip()
+            qm = qpat.match(line)
+            qa = apat.match(line)
+            if qm:
+                n, text = qm.groups()
+                current = [text]
+                questions.append(current)
+            elif qa:
+                text = qa.group(1)
+                current.append(text)
+            else:
+                print('Invalid line {i}: {line}')
+                break
+    return questions
+
+
+def load_weights(name='weights.dat'):
+    with open(name) as f:
+        wgts = [int(line.strip()) for line in f]
+    return wgts
+
+
+def save_weights(wgts, name='weights.dat'):
+    with open(name, 'w') as f:
+        for wgt in wgts:
+            print(wgt, file=f)
+
+
+def main(qname='questions.txt', sname='sorted.txt', wname='weights.dat'):
+
+    # Load the questions.
+    questions = load_questions(qname)
+
+    wfile = pathlib.Path(wname)
+    if wfile.exists():
+        # Load existing weights.
+        wgts = load_weights(wname)
+    else:
+        # Initialize and save new weights.
+        wgts = [5] * len(questions)
+        save_weights(wgts, wname)
+
+    # Pick a random question.
+    def nextq():
+        x = random.randint(0, sum(wgts) - 1)
+        cumsum = 0
+        for i, wgt in enumerate(wgts):
+            cumsum += wgt
+            if x < cumsum:
+                return i
+        assert False, 'should never get here'
+
+    # Loop over questions to ask.
+    n = 0
+    while True:
+        print(wgts)
+        n += 1
+        i = nextq()
+        Q = questions[i]
+        print(f'[{n}] {i}. {Q[0]}')
+        q = input('Hit RETURN to view the answer or q to quit: ')
+        if q == 'q':
+            # Save questions in order of decreasing weight.
+            isort = sorted(range(len(questions)), key=lambda i: wgts[i])
+            with open(sname, 'w') as f:
+                for j in isort:
+                    Q = questions[j]
+                    print(f'[{j + 1}](wgt={wgts[j]}) {Q[0]}', file=f)
+                    for ans in Q[1:]:
+                        print(f' - {ans}', file=f)
+            break
+        # Print the answers.
+        for a in Q[1:]:
+            print(f' - {a}')
+        w0 = wgts[i]
+        w = input(f'Enter the new weight 0-9 for this question or RETURN for {w0}: ')
+        if w == '':
+            w = w0
+        wgts[i] = int(w)
+        # Save the weights after each question.
+        shutil.copy(wname, wname + '.bak')
+        save_weights(wgts, wname)
+
+
+if __name__ == '__main__':
+    main()
